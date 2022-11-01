300409b0bc9138b06b3e0b0bd6610c41b4ec9fbf
luskas8 <gnomo1049@gmail.com>
2022-10-31 23:23:00 -0300

    [ADD] contador de bolões criado

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -2,5 +2,6 @@ namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
     PORT: number;
+    DATABASE_URL: string;
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
+import { PrismaClient } from '@prisma/client'
 import { config } from 'dotenv'
 import Fastify from 'fastify'
 config()
 
+const prisma = new PrismaClient({
+  log: ['query']
+})
+
 async function bootstrap () {
   const fastify = Fastify({
     logger: process.env.NODE_ENV === 'dev'
+  })
+
+  fastify.get('/pools/count', async () => {
+    const pools = await prisma.pool.count()
+    return { count: pools }
   })
 
   await fastify.listen({ port: process.env.PORT || 3333 })
