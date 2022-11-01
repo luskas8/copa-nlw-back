142cc01501adabdb5f73881a9cdd66fa7bc28c60
luskas8 <gnomo1049@gmail.com>
2022-11-01 20:08:34 -0300

    [ADD] estrutura do banco de dados, seed

---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: "John Doe",
+            email: "john.doe@gmail.com",
+            avatarUrl: "https://github.com/luskas8.png",
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: "Example Poll",
+            code: "BOL123",
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-10T15:45:00.987Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-11-05T15:45:00.987Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+            guesses: {
+                create: {
+                    firstTeamPoints: 2,
+                    secondTeamPoints: 1,
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id,
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
