5602e42015b1e8d24547f0122fdf4424d8b80c8a
luskas8 <gnomo1049@gmail.com>
2022-10-31 23:51:58 -0300

    [ADD] cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 import { config } from 'dotenv'
 import Fastify from 'fastify'
 config()
@@ -12,12 +13,16 @@ async function bootstrap () {
     logger: process.env.NODE_ENV === 'dev'
   })
 
+  await fastify.register(cors, {
+    origin: true
+  })
+
   fastify.get('/pools/count', async () => {
     const pools = await prisma.pool.count()
     return { count: pools }
   })
 
-  await fastify.listen({ port: process.env.PORT || 3333 })
+  await fastify.listen({ port: process.env.PORT || 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
